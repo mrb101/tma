1d84003c5ef60b7f37f94440dd73ecc1baf69a32
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2017-04-05 14:32:56 +0800

    fixes duplicate

--- tmaplatform/wsgi.py
+++ tmaplatform/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmaplatform.settings")
-application = DjangoWhiteNoise(application)
 
 application = get_wsgi_application()
 
