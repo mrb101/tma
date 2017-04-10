1a593551b9299ec50f14c94bd924dfaa5524820f
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2017-04-10 14:48:27 +0800

    adds json import

--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import json
+
 from .models import (
     MainBody,
     Clients,
